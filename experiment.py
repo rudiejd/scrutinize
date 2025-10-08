80d25ec3485ab734f440e8290c5d6323de9a06f6
JD <jd@rudiejd.io>
2025-10-08 15:39:23 -0400

    feat: implement the first four rules

--- experiment.py
+++ experiment.py
@@ -34,12 +34,26 @@ def _():
     vp_feed.ParseFromString(vehicle_positions.content)
     tu_feed = FeedMessage()
     tu_feed.ParseFromString(trip_updates.content)
-    return tu_feed, vp_feed
+    return requests, trip_updates, tu_feed, vehicle_positions, vp_feed
 
 
 @app.cell
 def _(mo):
     mo.md(r"""## Download GTFS-static schedule""")
+    return
+
+
+@app.cell
+def _(duckdb, requests):
+    from io import BytesIO, StringIO
+    from zipfile import ZipFile
+
+    static = requests.get("https://cdn.mbta.com/MBTA_GTFS.zip")
+
+    with ZipFile(BytesIO(static.content)) as thezip:
+        thezip.extractall("build/")
+
+    duckdb.query("CREATE TABLE trips AS SELECT * FROM read_csv('build/trips.txt', sample_size=-1)")
     return
 
 
@@ -71,29 +85,82 @@ def _(tu_feed, vp_feed):
 
     duckdb.query("CREATE OR REPLACE TABLE vehicle_positions AS (SELECT * FROM (SELECT unnest($data.entity, recursive := true)))", params={"data": vp_dict})
     duckdb.query("CREATE OR REPLACE TABLE trip_updates AS (SELECT * FROM (SELECT unnest($data.entity, recursive := true)))", params={"data": tu_dict})
-
-    return (duckdb,)
+    return duckdb, tu_dict
 
 
 @app.cell
-def _(duckdb):
+def _(mo):
+    mo.md(
+        r"""
+    ## Evaluate Rules
+    All of the below queries return rows for which the rule is not satisfied. If there are no rows, the rule passes.
+
+    This covers everything except for alerts right now
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### E001 Not in POSIX time""")
+    return
+
+
+@app.cell
+def _(mo, trip_updates):
     # E001 - Not in POSIX time
-    duckdb.query("SELECT * FROM trip_updates WHERE timestamp = 0")
-    return
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""## Evaluate Rules""")
-    return
-
-
-@app.cell
-def _(mo):
     _df = mo.sql(
         f"""
-        -- E002 - stop_time_updates not strictly sorted
-        SELECT * FROM
+        -- E001 - Not in POSIX time
+        -- trip_update.timestamp
+        -- stop_time_update.arrival.time / stop_time_update.departure.time
+        SELECT 
+            TO_TIMESTAMP(timestamp::DOUBLE) tu_timestamp,
+            -- vehicle ID
+            id,
+            tripId
+        FROM trip_updates
+        WHERE  
+
+         -- non-posix STU date
+         LENGTH(LIST_FILTER(stopTimeUpdate, 
+            	LAMBDA stu : 
+                    stu.scheduleRelationship != 'SKIPPED' AND
+            		(DATE(TO_TIMESTAMP(stu.departure.time::DOUBLE)) != CURRENT_DATE() OR
+            		DATE(TO_TIMESTAMP(stu.arrival.time::DOUBLE)) != CURRENT_DATE())
+             )) > 0 OR
+          -- non-posix trip_update.timestamp 
+         DATE(tu_timestamp) != CURRENT_DATE()
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo, tu_dict):
+    _df = mo.sql(
+        f"""
+        -- header.timestamp
+        SELECT "header.timestamp"
+        WHERE DATE(TO_TIMESTAMP({tu_dict['header']['timestamp']}::DOUBLE)) != CURRENT_DATE()
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo, vehicle_positions):
+    _df = mo.sql(
+        f"""
+        -- vehicle_position.timestamp
+        SELECT 
+            TO_TIMESTAMP(timestamp::DOUBLE) vp_timestamp,
+            id,
+            tripId
+        FROM vehicle_positions
+        WHERE 
+            DATE(vp_timestamp) != CURRENT_DATE()
         """
     )
     return
@@ -101,10 +168,64 @@ def _(mo):
 
 @app.cell
 def _(mo):
+    mo.md(r"""### E002 - stop_time_updates not strictly sorted""")
+    return
+
+
+@app.cell
+def _(mo, trip_updates):
     _df = mo.sql(
         f"""
-        -- E003 - GTFS-rt trip_id does not exist in GTFS data
-        SELECT * FROM
+        SELECT 
+        	id,
+        	tripId,
+            TO_TIMESTAMP(timestamp::DOUBLE) tu_timestamp,
+            LIST_TRANSFORM(stopTimeUpdate, LAMBDA stu: stu.stopSequence)
+        FROM trip_updates
+        WHERE  
+            -- https://github.com/duckdb/duckdb/discussions/16247
+            -- must be sorted
+            LIST_ZIP(
+            	LIST_TRANSFORM(stopTimeUpdate, LAMBDA stu: stu.stopSequence), 
+            	stopTimeUpdate)
+                .LIST_SORT()
+                .LIST_TRANSFORM(x -> x[2]) != stopTimeUpdate OR
+            -- no duplicates (strictly increasing)
+            LENGTH(LIST_DISTINCT(LIST_TRANSFORM(stopTimeUpdate, LAMBDA stu : stu.stopSequence))) != LENGTH(stopTimeUpdate)
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### E003 - GTFS-rt trip_id does not exist in GTFS data""")
+    return
+
+
+@app.cell
+def _(mo, trips, vehicle_positions):
+    _df = mo.sql(
+        f"""
+        -- vehicle positions
+        SELECT *, TO_TIMESTAMP(timestamp::DOUBLE) FROM vehicle_positions
+        LEFT JOIN trips t ON tripId = t.trip_id
+        WHERE t.trip_id IS NULL
+        AND scheduleRelationship = 'SCHEDULED'
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo, trip_updates, trips):
+    _df = mo.sql(
+        f"""
+        -- trip updates
+        SELECT *, TO_TIMESTAMP(timestamp::DOUBLE) FROM trip_updates
+        LEFT JOIN trips t ON tripId = t.trip_id
+        WHERE t.trip_id IS NULL
+        AND scheduleRelationship = 'SCHEDULED'
         """
     )
     return
